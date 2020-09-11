e360ee98189fa2f242b7ce425850f8df8c37366d
n1ceakhtar <niceakhtar21@gmail.com>
2020-09-11 20:49:40 +0530

    Passport

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport')
+var authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/userRouter');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
@@ -31,26 +33,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
+  if(!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
       return next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
       next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
   }
 }
 
