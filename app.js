1d14628385e17d31b9e59c3ecc29d29d6194b7cb
n1ceakhtar <niceakhtar21@gmail.com>
2020-09-11 11:45:41 +0530

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,16 +22,24 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 //auth handler
 function auth (req, res, next) {
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
+        res.setHeader('WWW-Authenticate', 'Basic');                        
         err.status = 401;
         next(err);
         return;
@@ -38,17 +48,18 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
+        req.session.user = 'admin';
         next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
+        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
         next(err);
     }
   }
   else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
+          console.log('req.session: ',req.session);
           next();
       }
       else {
